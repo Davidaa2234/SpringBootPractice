39be9b2c023a10cf0ffe96ffb2bb700358c281b9
David Lin <a2775255@gmail.com>
2026-03-12 01:44:36 +0800

    first time

---
+++ src/main/java/com/example/demo/SpringbootMvcApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot 程式進入點
+@SpringBootApplication
+public class SpringbootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcApplication.class, args);
+	}
+
+}
